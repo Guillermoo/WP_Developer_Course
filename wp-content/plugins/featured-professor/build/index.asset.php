2d887033f89990199001d7e94562c0f2113c6928
Guillermo <guillermo.cano.es@gmail.com>
2021-10-08 14:02:17 +0200

    CreaDB y filtrar #1

--- wp-content/plugins/featured-professor/build/index.asset.php
+++ wp-content/plugins/featured-professor/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '243133a21fe66883b803986f091e453e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-polyfill'), 'version' => '2354e46fd113196e5e01a002a4ef1f9c');
